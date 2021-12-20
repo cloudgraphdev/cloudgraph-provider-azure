4480ebe0ca65c192cc99804565a00d1e5d74a4ca
James Zhou <james.zhou@inspire11.com>
2021-12-20 11:13:32 -0600

    feat(CG-401) add storage account - container relation

--- src/enums/relations.ts
+++ src/enums/relations.ts
@@ -3,6 +3,7 @@
  * The key of the object represents the parent or base service,
  * it might contain a array of dependant or childs that must be executed after the parent
  */
- export default {
-  resourceGroup: ['azureFunction']
+export default {
+  resourceGroup: ['azureFunction'],
+  storageAccount: ['storageContainer'],
 }
